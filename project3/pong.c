769edebe72b844bd1ab09e26abebb66fe9ce9f19
httpsteven <httpstevenlul@gmail.com>
2023-12-11 01:17:31 -0700

    more

--- project3/pong.c
+++ project3/pong.c
@@ -1,175 +1,136 @@
 #include <msp430.h>
 #include <libTimer.h>
-#include "lcdutils.h"
-#include "lcddraw.h"
+#include <lcdutils.h>
+#include <lcddraw.h>
 
-// WARNING: LCD DISPLAY USES P1.0.  Do not touch!!!
-
-#define LED BIT6 /* note that bit zero req'd for display */
-
+#define LED BIT6
 #define SW1 1
 #define SW2 2
 #define SW3 4
 #define SW4 8
-
 #define SWITCHES 15
+#define SW0 BIT3
 
-char blue = 31, green = 0, red = 31;
-unsigned char step = 0;
+// ...
 
-static char switch_update_interrupt_sense()
-{
-    char p2val = P2IN;
-    P2IES |= (p2val & SWITCHES);
-    P2IES &= (p2val | ~SWITCHES);
-    return p2val;
-}
-
-void switch_init()
-{
-    P2REN |= SWITCHES;
-    P2IE |= SWITCHES;
-    P2OUT |= SWITCHES;
-    P2DIR &= ~SWITCHES;
+void sw_init(char port, int switches, int* paddle_dir) {
+  P1REN |= switches;
+  P1IE |= switches;
+  P1OUT |= switches;
+  P1DIR &= ~switches;
+  P1IES |= (P1IN & switches);
+  P1IES &= (P1IN | ~switches);
+  if (port == '1')
+    sw0_update_interrupt_sense();
+  else if (port == '2')
     switch_update_interrupt_sense();
 }
 
-int switches = 0;
-
-void switch_interrupt_handler()
-{
-    char p2val = switch_update_interrupt_sense();
-    switches = ~p2val & SWITCHES;
+void init_paddle(int* paddle_pos, int x, int y) {
+  paddle_pos[0] = x;
+  paddle_pos[1] = y;
 }
 
-short drawPos[2] = {1, 10}, controlPos[2] = {2, 10};
-short colVelocity = 1, colLimits[2] = {1, screenWidth / 2};
-
-void draw_ball(int col, int row, unsigned short color)
-{
-    fillRectangle(col - 1, row - 1, 3, 3, color);
+void draw_game_objects() {
+  drawRect(t_paddle_pos, paddle_dims, COLOR_WHITE);
+  drawRect(b_paddle_pos, paddle_dims, COLOR_WHITE);
+  drawRect(ball_pos, ball_dims, COLOR_WHITE);
 }
 
-void screen_update_ball()
-{
-    for (char axis = 0; axis < 2; axis++)
-        if (drawPos[axis] != controlPos[axis])
-            goto redraw;
-    return;
-redraw:
-    draw_ball(drawPos[0], drawPos[1], COLOR_BLUE);
-    for (char axis = 0; axis < 2; axis++)
-        drawPos[axis] = controlPos[axis];
-    draw_ball(drawPos[0], drawPos[1], COLOR_WHITE);
+void update_game_objects(int* pos, int* dims, int* dir, int bg_color, int obj_color) {
+  drawRect(pos, dims, bg_color);
+  pos[0] += dir[0];
+  pos[1] += dir[1];
+  drawRect(pos, dims, obj_color);
 }
 
-short redrawScreen = 1;
-u_int controlFontColor = COLOR_GREEN;
+int ranges_overlap(int r1[], int r2[]) {
+  return (r1[0] <= r2[1] && r1[0] >= r2[0]) || (r2[0] <= r1[1] && r2[0] >= r1[0]);
+}
 
-void wdt_c_handler()
-{
-    static int secCount = 0;
+int boxes_collide(int b1pos[], int b1dims[], int b2pos[], int b2dims[]) {
+  int b1_bounds_horiz[] = {b1pos[0], b1pos[0] + b1dims[0]};
+  int b2_bounds_horiz[] = {b2pos[0], b2pos[0] + b2dims[0]};
+  int horiz_overlap = ranges_overlap(b1_bounds_horiz, b2_bounds_horiz);
 
-    secCount++;
-    if (secCount >= 25)
-    {
-        {
-            short oldCol = controlPos[0];
-            short newCol = oldCol + colVelocity;
-            if (newCol <= colLimits[0] || newCol >= colLimits[1])
-                colVelocity = -colVelocity;
-            else
-                controlPos[0] = newCol;
-        }
+  int b1_bounds_vert[] = {b1pos[1], b1pos[1] + b1dims[1]};
+  int b2_bounds_vert[] = {b2pos[1], b2pos[1] + b2dims[1]};
+  int vert_overlap = ranges_overlap(b1_bounds_vert, b2_bounds_vert);
 
-        {
-            if (switches & SW3)
-                green = (green + 1) % 64;
-            if (switches & SW2)
-                blue = (blue + 2) % 32;
-            if (switches & SW1)
-                red = (red - 3) % 32;
-            if (step <= 30)
-                step++;
-            else
-                step = 0;
-            secCount = 0;
-        }
-        if (switches & SW4)
-            return;
-        redrawScreen = 1;
+  return horiz_overlap && vert_overlap;
+}
+
+// ...
+
+int main() {
+  configureClocks();
+  lcd_init();
+  switch_init();
+  sw_init('1', SW0, t_paddle_dir);
+  sw_init('2', SWITCHES, b_paddle_dir);
+  enableWDTInterrupts();
+  or_sr(0x8);
+
+  int bg_clr = COLOR_BLACK;
+  int obj_clr = COLOR_WHITE;
+  clearScreen(bg_clr);
+  init_paddle(t_paddle_pos, 98, 10);
+  init_paddle(b_paddle_pos, 0, 145);
+  init_paddle(ball_pos, 20, 20);
+  draw_game_objects();
+
+  while (1) {
+    update_game_objects(t_paddle_pos, paddle_dims, t_paddle_dir, bg_clr, obj_clr);
+    update_game_objects(b_paddle_pos, paddle_dims, b_paddle_dir, bg_clr, obj_clr);
+    update_game_objects(ball_pos, ball_dims, ball_dir, bg_clr, obj_clr);
+
+    if ((ball_pos[0] <= 0) || (ball_pos[0] + ball_dims[0] >= display_dims[0])) {
+      ball_dir[0] = -ball_dir[0];
+      // TODO: beep
     }
-}
 
-void update_shape();
-
-void main()
-{
-    P1DIR |= LED;
-    P1OUT |= LED;
-    configureClocks();
-    lcd_init();
-    switch_init();
-
-    enableWDTInterrupts();
-
-    or_sr(0x8);
-
-    clearScreen(COLOR_BLUE);
-    while (1)
-    {
-        if (redrawScreen)
-        {
-            redrawScreen = 0;
-            update_shape();
-        }
-        P1OUT &= ~LED;
-        or_sr(0x10);
-        P1OUT |= LED;
+    if (boxes_collide(b_paddle_pos, paddle_dims, ball_pos, ball_dims)) {
+      ball_dir[1] = -1;
     }
-}
 
-void screen_update_hourglass()
-{
-    static unsigned char row = screenHeight / 2, col = screenWidth / 2;
-    static char lastStep = 0;
-
-    if (step == 0 || (lastStep > step))
-    {
-        clearScreen(COLOR_BLUE);
-        lastStep = 0;
+    if (boxes_collide(t_paddle_pos, paddle_dims, ball_pos, ball_dims)) {
+      ball_dir[1] = 1;
     }
-    else
-    {
-        for (; lastStep <= step; lastStep++)
-        {
-            int startCol = col - lastStep;
-            int endCol = col + lastStep;
-            int width = 1 + endCol - startCol;
 
-            unsigned int color = (blue << 11) | (green << 5) | red;
-
-            fillRectangle(startCol, row + lastStep, width, 1, color);
-            fillRectangle(startCol, row - lastStep, width, 1, color);
-        }
+    if (ball_pos[1] <= 0 || ball_pos[1] + ball_dims[1] >= display_dims[1]) {
+      clearScreen(COLOR_RED);
+      break;
     }
+
+    if (t_paddle_pos[0] <= 0 || t_paddle_pos[0] + paddle_dims[0] >= display_dims[0])
+      t_paddle_dir[0] = 0;
+
+    if (b_paddle_pos[0] <= 0 || b_paddle_pos[0] + paddle_dims[0] >= display_dims[0])
+      b_paddle_dir[0] = 0;
+  }
 }
 
-void update_shape()
-{
-    screen_update_ball();
-    screen_update_hourglass();
+void sw_interrupt_handler(char port, int* paddle_dir) {
+  char p_val = (port == '1') ? sw0_update_interrupt_sense() : switch_update_interrupt_sense();
+  switches = ~p_val & ((port == '1') ? SW0 : SWITCHES);
+
+  if (switches & SW0)
+    paddle_dir[0] = -2;
+  // ...
 }
 
-__interrupt_vec(PORT2_VECTOR) Port_2()
-{
-    if (P2IFG & SWITCHES)
-    {
-        P2IFG &= ~SWITCHES;
-        switch_interrupt_handler();
-    }
+void __interrupt_vec(PORT1_VECTOR) port_1() {
+  if (P1IFG & SW0) {
+    P1IFG &= ~SW0;
+    sw_interrupt_handler('1', t_paddle_dir);
+  }
 }
 
-__interrupt_vec(WDT_VECTOR) WDT_ISR()
-{
-    // Empty ISR since the actual WDT
+void __interrupt_vec(PORT2_VECTOR) Port_2() {
+  if (P2IFG & SWITCHES) {
+    P2IFG &= ~SWITCHES;
+    sw_interrupt_handler('2', b_paddle_dir);
+  }
+}
+
+// ...
